2c202b0da58e4db853e904dd52de6afa60d64dae
user <user@localhost.localdomain>
2023-08-16 18:09:57 +0300

    Test curses

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import curses
+import time
 
 
 def main(stdscr: curses.window):
+    curses.update_lines_cols()
     stdscr.clear()
     curses.noecho()
     curses.cbreak()
+    curses.curs_set(False)
     stdscr.keypad(True)
+
+    stdscr.addstr(5, 20, "Pretty text")
+    stdscr.border()
+    stdscr.refresh()
+    time.sleep(3)
+
     curses.endwin()
+
+
+def game_loop():
+    pass
 
 
 if __name__ == "__main__":
